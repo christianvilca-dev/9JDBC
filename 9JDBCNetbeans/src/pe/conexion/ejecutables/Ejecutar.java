3991512f0a1f261ab14e7ac56aaa42776561fcf7
Christian <Christian@Tom>
2018-10-04 17:25:36 -0500

    Pruebas

--- 9JDBCNetbeans/src/pe/conexion/ejecutables/Ejecutar.java
+++ 9JDBCNetbeans/src/pe/conexion/ejecutables/Ejecutar.java
@@ -22,14 +22,13 @@ public class Ejecutar {
     public static void main(String[] args) {
         
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Seleccione la BD que va a usar");
-        System.out.println("1. Postgresql");
-        System.out.println("2. MySQL");
-        int opcion = teclado.nextInt();
-        
         UsuarioDAO userDAO = null;
+        byte opcion;
         
-        switch (opcion){
+        mostrarOpcionBases();
+        opcion = teclado.nextByte();
+        
+        switch(opcion) {
             case 1:
                 userDAO = new PSQLUsuario();
                 break;
@@ -38,43 +37,64 @@ public class Ejecutar {
                 break;
             default:
                 System.out.println("No es una opcion valida");
+                System.exit(0);
         }
         
-        Usuario user = new Usuario();
-        user.setIdUsuario(2);
-        user.setUsuario("otro");
-        user.setClave("otro");
-        user.setCorreo("otro@gmail.com");
+        mostrarOpcionCRUD();
+        opcion = teclado.nextByte();
         
+        switch (opcion) { 
+            case 1:
+                insertarUsuario(userDAO);
+		break;
+            case 2:
+		listarUsuarios(userDAO);
+		break;
+		default:
+		System.out.println("Opcion no valida!!!");
+		System.exit(0);
+        }
+    }
+
+    private static void mostrarOpcionBases() {
+        System.out.println("************************************");
+        System.out.println("** Seleccione la BD que va a usar **");
+        System.out.println("** 1. Postgresql                  **");
+        System.out.println("** 2. MySQL                       **");
+        System.out.println("************************************");
+        System.out.print("   Digite su opción:  ");
+    }
+    
+    private static void mostrarOpcionCRUD() {
+        System.out.println("************************************");
+        System.out.println("** Seleccione opcion de ejecutar  **");
+        System.out.println("** 1. Insertar                    **");
+        System.out.println("** 2. Listar                      **");
+        System.out.println("************************************");
+        System.out.print("   Digite su opción:  ");
+    }
+
+    private static void insertarUsuario(UsuarioDAO userDAO) {
+        Usuario usuario = new Usuario();
+        usuario.setUsuario("Pedro");
+        usuario.setClave("MuySegura");
+        usuario.setCorreo("pedro@gmail.com");
         try {
-            // No importa si es una BD relacional o no relacional
-            userDAO.insert(user);
-//            List<Usuario> listado = userDAO.listar();
-//            for (Usuario u : listado) {
-//                System.out.println(u);
-//            }
+            userDAO.insert(usuario);
+            System.out.println("Usuario insertado con el ID: " + usuario.getIdUsuario());
         } catch (ExcepcionGeneral eg) {
             System.out.println(eg.getMessage());
         }
-        
-//        PSQLUsuario psqluser = new PSQLUsuario();
-//        try {
-//            psqluser.insert(user);
-//            System.out.println("Usuario registrado con el id: " + user.getIdUsuario());
-//        } catch (ExcepcionGeneral eg) {
-//            System.out.println(eg.getMessage());
-//        }
-        
-        
-//        MySQLUsuario mysqluser = new MySQLUsuario();
-//        try {
-//            List<Usuario> listado = mysqluser.listar();
-//            
-//            for (Usuario u : listado) {
-//                System.out.println(u);
-//            }
-//        } catch (ExcepcionGeneral eg) {
-//            System.out.println(eg.getMessage());
-//        }
+    }
+
+    private static void listarUsuarios(UsuarioDAO userDAO) {
+        try {
+            List<Usuario> listado = userDAO.listar();
+            for (Usuario usuario : listado) {
+                System.out.println(usuario);
+            }
+        } catch (ExcepcionGeneral eg) {
+            System.out.println(eg.getMessage());
+        }
     }
 }
