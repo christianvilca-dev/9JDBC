82ae8df229ffb613ad8487c00d62cd88bad44fcf
Christian <Christian@Tom>
2018-10-03 20:32:10 -0500

    Clase Usuario - Interface generica y de usuario

---
+++ 9JDBCNetbeans/src/pe/conexion/ado/interfaces/DAO.java
@@ -0,0 +1,19 @@
+/*
+ * Interfaz general que nos permite realizar nuestro proceso independiente
+ * de cual BD se esta trabajando
+ */
+package pe.conexion.ado.interfaces;
+
+import java.util.List;
+
+/**
+ *
+ * @author Christian
+ */
+public interface DAO<K, O> {
+    public void insert(O o);
+    public void modificar(O o);
+    public void eliminar(O o);
+    public void obtenerPorId(K k);
+    public List<O> listar();
+}
---
+++ 9JDBCNetbeans/src/pe/conexion/ado/interfaces/UsuarioDAO.java
@@ -0,0 +1,14 @@
+/*
+ * Here
+ */
+package pe.conexion.ado.interfaces;
+
+import pe.conexion.modelos.Usuario;
+
+/**
+ *
+ * @author Christian
+ */
+public interface UsuarioDAO extends DAO<Integer, Usuario>{
+    
+}
